ed64004d146ef796d2695e4fca99dc4d93690a3c
Dalton <daltonvaren@gmail.com>
2018-05-18 16:44:58 -0700

    relinked jquery and verified it was working

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,13 @@
-console.log('Im working!');
+$(document).ready(function() {
 
+console.log('Im working!');
+//function to call particles.json
 particlesJS.load('particles-js', 'scripts/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
+
+$('.homeLnk').on('click',function(){
+  console.log('clicked jQuery works');
+})
+
+});
