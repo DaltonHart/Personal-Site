c258c5bb8b7b7aff5b8f44ebbbd04ecd6dfcc8b5
Dalton <daltonvaren@gmail.com>
2018-05-19 15:36:57 -0700

    added google api and map into contact section. Also changed structure to look like comic book style

--- scripts/app.js
+++ scripts/app.js
@@ -9,5 +9,31 @@ particlesJS.load('particles-js', 'scripts/particles.json', function() {
 $('.homeLnk').on('click',function(){
   console.log('clicked jQuery works');
 })
+//scroll function
+$(".navBar").find("a").on('click',function(e) {
+  e.preventDefault();
+  console.log('clicked the nav bar');
+  var section = $(this).attr("href");
+  $("html, body").animate({
+      scrollTop: $(section).offset().top
+  });
+});
+
+
+//google maps api to show sf area 
+var map;
+var googleMap = function () {
+  console.log('map!');
+    map = new google.maps.Map(document.getElementById("contact-me"), {
+      center: {lat: 37.790921, lng: -122.401219},
+      zoom: 18
+    });
+    marker = new google.maps.Marker({
+      position: map.center,
+      map: map,
+    })
+}
+
+googleMap();
 
 });
