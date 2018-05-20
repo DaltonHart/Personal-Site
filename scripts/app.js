7e4c5d2acda5bbfbb8c0f0c0e3051dbb0dccd3f6
DaltonHart <daltonvaren@gmail.com>
2018-05-20 15:29:01 -0700

    created testimonies section out of javascript object

--- scripts/app.js
+++ scripts/app.js
@@ -42,6 +42,61 @@ function onScroll(event){
 //call function
 $(document).on('scroll',onScroll);
 
+// Scripts for project area carosel 
+
+
+
+
+// Scripts for Sponsor area 
+//array of sponsor objects
+var sponsors = [
+  {
+    name: 'Khahn Tran',
+    relation: 'associate',
+    message: 'Dalton’s kindness and intelligence is beyond anyone that I’d known. He is like a brother that we can go to when we need help. He outshines everyone in our workplace and is always thriving to better himself. I personally believe Dalton knows everything about technology, ever since I met Dalton, I no longer have to google search tech questions. As a manager , he is known to us as the to-go guy, which mean we go to him for everything and he loves it! It’s a blessing to have such a wonderful person in our life!',
+    img: '/assets/khahn.jpg'
+  },
+  {
+    name: 'Giselle Canada',
+    relation: 'Coworker',
+    message: 'After everything already said about Dalton it’s clear to see the impact he has had and continues to have on so many different people. What you may not know is he recently moved from his hometown to the Bay Area, taking a huge leap of faith with his career and even through all of that stress and transition he still has had the same impact on a whole new group of people, including me. When I met him I instantly respected and admired him just by the way he carries himself. As my close peer, he drives me to be the best so I can maybe be as great as him one day! He’s extremely dependable and consistent, if anyone is looking for an amazing investment look no further than Dalton!',
+    img:'/assets/giselle.jpg',
+  },
+  {
+    name: 'Duncan Nankervis',
+    relation: 'Friend',
+    message: "I met Dalton through my girlfriend 4 years ago, and in those short 4 years I have gained a brother. I owe a lot to my friend, Dalton. He is a selfless, generous, and wonderful individual. He has one of the best work-ethics I have ever seen, and goes full speed into helping others achieve their dreams. If there is one person I know that deserves the opportunity to follow his own dreams for a change, it would be Dalton, one of the most important people I have the blessing to have in my life. I endorse this guy 100%, his word is good, and his intentions pure. Lets give him the chance, because a chance is all he needs to be successful.",
+    img: '/assets/duncan.jpg',
+  },
+  {
+    name: 'Jessica Amber',
+    relation: 'Friend',
+    message: "Dalton has done so much for me, that I can't even put it all to words. He is the most understanding, forgiving, kind, and honest man I have ever met, and I will do whatever I can to make sure that he can get to where he wants to be. Dalton is the kind of man who, no matter how difficult the problem, will not only find the solution, but teach you how to find the solution as well. He has never given up on anyone or anything, because he knows that there is always a work around, or a different way to look at the situation.",
+    img: 'assets/jessica.jpg',
+  },
+]
+
+//call below function
+changeSponsor();
+// function to change elements on sponsor page
+function changeSponsor(){
+// itterates through the array of sponsors
+    $.each(sponsors,function(i, sponsor){
+      //sets a time out sequence for delay
+        setTimeout(function(){
+          $('.sponsor img').attr('src', sponsors[i].img);
+          $('.sponsor-comment ul li:first-child').text(`Name: ${sponsors[i].name}`);
+          $('.sponsor-comment ul li:nth-child(2)').text(`Relation: ${sponsors[i].relation}`);
+          $('.sponsor-comment ul li:nth-child(3)').text(`"${sponsors[i].message}"`);
+          
+        }, i * 8000);
+    });
+}
+
+
+
+
+
 /*Scripts for Social area*/
 //google maps api to show sf area in Contact area
 var map;
